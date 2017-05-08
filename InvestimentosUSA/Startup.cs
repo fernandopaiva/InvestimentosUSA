69f58ea8470c9b454892433f23ae3cda48e33c94
fernandopaiva <fernandopaivabr@gmail.com>
2017-05-08 19:17:52 -0300

    first commit

---
+++ InvestimentosUSA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InvestimentosUSA.Startup))]
+namespace InvestimentosUSA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
